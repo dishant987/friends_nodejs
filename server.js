a4c76c8bb4fe3c213ab0980ff0f52eab28cf7c4e
dishant987 <tankdishant2003@gmail.com>
2024-04-03 21:38:20 +0530

    change

--- server.js
+++ server.js
@@ -1,40 +1,45 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import { config } from 'dotenv';
-import connect from './database/conn.js';
-import router from './router/route.js';
-const app = express()
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { config } from "dotenv";
+import connect from "./database/conn.js";
+import router from "./router/route.js";
 
-const corsOptions = {
-    origin: 'https://react-friends-fn72.onrender.com',
-  };
-
-app.use(morgan('tiny'));
-app.use(cors(corsOptions));
-app.use(express.json());
-config();
-
+const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-    try {
-        res.json("Get Request")
-    } catch (error) {
-        res.json(error)
-    }
-})
+// Load environment variables
+config();
 
-app.use('/api',router);
+// Connect to the database
+connect()
+  .then(() => {
+    console.log("Database connected successfully");
+    // Start the server after successful database connection
+    app.listen(port, () => {
+      console.log(`Server connected to http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+    // Terminate the process if database connection fails
+    process.exit(1);
+  });
 
-connect().then(() => {
-    try {
-        app.listen(port, () => {
-            console.log(`Server connected to http://localhost:${port}`)
-        })
-    } catch (error) {
-        console.log("Cannot connect to the server");
-    }
-}).catch(error => {
-    console.log("Invalid Database Connection");
-})
+// Middleware
+app.use(morgan("tiny")); // Logging middleware
+app.use(cors({ origin: "https://react-friends-fn72.onrender.com" })); // CORS middleware
+app.use(express.json()); // Body parser middleware
+
+// Routes
+app.get("/", (req, res) => {
+  res.json("Get Request");
+});
+
+app.use("/api", router); // Use router for API routes
+
+// Error handler middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Internal Server Error");
+});
